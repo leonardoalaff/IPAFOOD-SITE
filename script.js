528bfa9f3aaefb8a84c06ae83d4687955d5c12fe
Leonardo Alaff <110346180+leonardoalaff@users.noreply.github.com>
2025-03-09 01:12:22 -0300

    btn dark theme add

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const menu = document.querySelector('.menu')
+const btnTema = document.querySelector('.btn-tema')
+const ball = document.querySelector('.ball')
+const body = document.querySelector('body')
+const textoApresentacao = document.querySelector('.texto-apresentacao')
+const iconeMenu = document.querySelector('.icone-menu')
 
 menuMobile.addEventListener('click', () => {
     menu.classList.toggle('active')
     menuMobile.classList.toggle('active')
+})
+
+btnTema.addEventListener('click', ()=> {
+    btnTema.classList.toggle('active')
+    ball.classList.toggle('active')
+    body.classList.toggle('active')
+    textoApresentacao.classList.toggle('active')
+    menu.classList.toggle('active2')
+    iconeMenu.classList.toggle('active')
+    menuMobile.classList.toggle('active2')
 })
